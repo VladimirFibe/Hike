324f1344a2a430276922c5ba5bfdc7ccab4520dc
Vladimir Fibe <motiw@icloud.com>
2023-08-20 15:23:10 +0600

    add title

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -4,10 +4,28 @@ struct CardView: View {
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            content
+            VStack {
+                title
+                content
+            }
         }
         .frame(height: 570)
         .padding(.horizontal)
+    }
+    var title: some View {
+        Text("Hiking")
+            .fontWeight(.black)
+            .font(.system(size: 52))
+            .foregroundStyle(
+                LinearGradient(
+                    colors: [
+                        .colorGrayLight,
+                        .colorGrayMedium
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
     }
     var content: some View {
         ZStack {
