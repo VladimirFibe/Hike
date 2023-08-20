2bf94c8ca42fcde99967ed85e0b9823cb87a1fd1
Vladimir Fibe <motiw@icloud.com>
2023-08-20 14:00:41 +0600

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Vladimir Fibe on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
