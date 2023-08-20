db4ddcdca29a01ca8512bb7408b95aee5ca859c2
Vladimir Fibe <motiw@icloud.com>
2023-08-20 14:12:27 +0600

    add image-1

--- Hike/ContentView.swift
+++ Hike/ContentView.swift
@@ -1,21 +1,10 @@
-//
-//  ContentView.swift
-//  Hike
-//
-//  Created by Vladimir Fibe on 20.08.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
     }
 }
 
