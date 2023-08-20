30af87e5dbd6561793096230b5e02fb4d31b53eb
Vladimir Fibe <motiw@icloud.com>
2023-08-20 17:14:45 +0600

    Button Style

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -1,16 +1,42 @@
 import SwiftUI
 
 struct CardView: View {
+    @State private var imageNumber = 1
+    private func randomImage() {
+        let random = Int.random(in: 1...5)
+        imageNumber = random == imageNumber ? random == 5 ? 1 : random + 1 : random
+    }
+
     var body: some View {
         ZStack {
             CustomBackgroundView()
             VStack {
                 header
                 content
+                footer
             }
         }
         .frame(height: 570)
         .padding(.horizontal)
+    }
+    var footer: some View {
+        Button(action: randomImage) {
+            Text("Explore More")
+              .font(.title2)
+              .fontWeight(.heavy)
+              .foregroundStyle(
+                LinearGradient(
+                  colors: [
+                    .colorGreenLight,
+                    .colorGreenMedium
+                  ],
+                  startPoint: .top,
+                  endPoint: .bottom
+                )
+              )
+              .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+        }
+        .buttonStyle(GradientButton())
     }
     var header: some View {
         VStack(alignment: .leading) {
@@ -57,9 +83,10 @@ struct CardView: View {
                     )
                 )
                 .frame(width: 256, height: 256)
-            Image("image-1")
+            Image("image-\(imageNumber)")
                 .resizable()
                 .scaledToFit()
+                .animation(.default, value: imageNumber)
         }
     }
 }
